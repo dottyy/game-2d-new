b0d8918b7e95f9423d3b81880f6fc89b161d37fb
dottyy <dottykurmi123@gmail.com>
2018-10-08 17:48:43 -0400

    agragar personajes y plataforma

---
+++ gamed2/Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public GameObject jugador;
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(jugador.transform.position.x, jugador.transform.position.y, jugador.transform.position.z-10);
+	}
+}
